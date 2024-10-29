1adc23a3937b9fdbdbf6964ba18d2a2c6d829760
yehuo <1978002026@qq.com>
2024-10-29 22:13:21 +0800

    针对配置文件的敏感信息进行加密

--- src/main/java/com/yehuo/githubdatabackend/GithubDataBackendApplication.java
+++ src/main/java/com/yehuo/githubdatabackend/GithubDataBackendApplication.java
@@ -1,8 +1,9 @@
 package com.yehuo.githubdatabackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableEncryptableProperties
 @SpringBootApplication
 public class GithubDataBackendApplication {
 
